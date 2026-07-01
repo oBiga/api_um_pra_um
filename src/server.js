ab7455091db2f9d6f1a3f4a6c6b850cf2db59d65
oBiga <kallany_senabio@estudante.sesisenai.org.br>
2026-07-01 08:49:04 -0300

    commitado

--- src/server.js
+++ src/server.js
@@ -16,7 +16,8 @@ app.get("/usuarios", async (req, res) => {
     }
 })
 
-app.POST("/usuarios", async(req, res) =>{
+app.post("/usuarios", async(req, res) =>{
+    try{
     const {email, senha, bio, fotoUrl} = req.body;
     const novoUsuario = await prisma.usuario.create({
         data: {
@@ -28,6 +29,59 @@ app.POST("/usuarios", async(req, res) =>{
         },
         include: {perfil: true}
         })
+    console.log("usuario Criado")
+    res.status(201).json(novoUsuario)
+    }
+    catch(error){
+        res.status(400).json(NIGGA)
+    }
+})
+
+app.put("/usuarios/:id", async(req, res) =>{
+    const {id} = req.params
+try{
+    const {email, senha, bio, fotoUrl} = req.body;
+
+    const usuarioAtualizao = await prisma.usuario.update({
+            where: {id: Number(id)},
+    data:{
+        email,
+        senha,
+        perfil:{
+            upsert:{
+                create:{bio, fotoUrl},
+                update: {bio, fotoUrl}
+            }
+        }
+    },
+    include: {perfil: true}
+    })
+    res.json(usuarioAtualizado);
+} catch (error) {
+    console.error("error ao atualizar o usuario")
+    res.status(400).json({ erro: "error a atualizar o usuario"})
+
+}
+})
+
+app.patch("/usuarios/:id", async(req, res) =>{
+    const {id} = req.params
+
+    const {email, senha, bio, fotoUrl} = req.body
+
+    const dadosAtualizados = {
+        email,
+        senha,
+        perfil: (bio || fotoUrl) ?{
+            update: {bio, fotoUrl}
+        } : undefined
+    }
+    Object.keys(dadosAtualizados).forEach(key => dadosAtualizaos[key] === undefined && delete dadosAtualizados[key])
+    const uduario = await prisma.usuario.update({
+        where:  {id: Number(id)},
+        data: dadosAtualizacao,
+        include: {perfil: true}
+    });
 })
 
 app.listen(PORT, () =>{
